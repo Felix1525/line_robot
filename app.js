18df0d330197f802bf69e92c97985fddf31083f1
Felix <44264272+Felix1525@users.noreply.github.com>
2023-04-01 09:21:43 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
+// 載入模組
 require('dotenv').config();
 
-const express = require('express');
 const line = require('@line/bot-sdk');
-const { Configuration, OpenAIApi } = require("openai");
+const express = require('express');
+const { Configuration, OpenAIApi } = require('openai');
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-// create LINE SDK config from env variables
+// line token 和 密鑰
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
-// create LINE SDK client
+// openai key
+apiKey2 = process.env.OPENAI_API_KEY;
+
+// 建立line端
 const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
+// 建立伺服器
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
+// 存儲用戶會話的對象
+const userConversations = {};
+
+// 監聽路由
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
@@ -34,36 +34,77 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
-// event handler
+// 事件捕捉
 async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  const { data } = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+  // 文本內容傳去open ai chat3.5
+  const configuration = new Configuration({ apiKey: apiKey2 });
+  const openai = new OpenAIApi(configuration);
+
+  const openaiResponse = await openai.createChatCompletion({
+    // 模型選擇
+    model: 'gpt-3.5-turbo',
+
+    // 信息
     messages: [
-      {
-        role: 'user',
-        content: '今後的對話中，請你扮演我的全能助理，而你的名字是 AI，你會替我分析我的問題並給我一些建議與答案，你必須用繁體中文，以及台灣用語來回覆我，這些規則不需要我重新再說明。'
-      },
-      {
-        role: 'user',
-        content: event.message.text,
-      }
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: event.message.text },
     ],
+
+    // 回傳字數限制
     max_tokens: 2000,
+
+    // 溫度
+    temperature: 1,
   });
 
-  // create a echoing text message
-  const [choices] = data.choices;
-  const echo = { type: 'text', text: choices.message.content.trim() || '抱歉，我沒有話可說了。' };
+  const userId = event.source.userId;
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  if (!userConversations[userId]) {
+    userConversations[userId] = [
+      { role: 'system', content: 'Your are a helpful assistant.' },
+    ];
+  }
+
+  userConversations[userId].push({ role: 'user', content: event.message.text });
+
+  if (userConversations[userId].length > 6) {
+    userConversations[userId].shift();
+  }
+
+  if (event.message.text.slice(0, 2) === '畫圖') {
+    const response = await openai.createImage({
+      prompt: event.message.text,
+      n: 1,
+      size: '1024x1024',
+    });
+
+    const imageUrl = response.data.data[0].url;
+
+    const imageMessage = {
+      type: 'image',
+      originalContentUrl: imageUrl,
+      previewImageUrl: imageUrl,
+    };
+
+    return client.replyMessage(event.replyToken, imageMessage);
+  }
+
+  // 讀取回傳內容文本
+  const assistantReply = openaiResponse.data.choices[0].message.content;
+
+  // 建立回傳內容
+  const reply = { type: 'text', text: assistantReply };
+
+  // 回傳去line
+  return client.replyMessage(event.replyToken, reply);
+
 }
-
+  
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
